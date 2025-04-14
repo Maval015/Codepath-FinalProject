a82b2ba62e2cc45daaba119ab1cfe4966f9e3773
Alex <Alexer13@gmail.com>
2025-04-14 15:40:18 -0400

    Initial Commit

---
+++ FitBuddy/FitBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitBuddyApp.swift
+//  FitBuddy
+//
+//  Created by michel avalos on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FitBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
